b66c10f813db04ec73ba31646fed0a06069fa346
AnnecyV <94387788+AnnecyV@users.noreply.github.com>
2021-12-11 14:40:56 +0100

    End of section 18

---
+++ Core/Entities/OrderAggregate/ProductItemOrdered.cs
@@ -0,0 +1,20 @@
+namespace Core.Entities.OrderAggregate
+{
+    public class ProductItemOrdered
+    {
+        public ProductItemOrdered()
+        {
+        }
+
+        public ProductItemOrdered(int productItemId, string productName, string pictureURL)
+        {
+            ProductItemId = productItemId;
+            ProductName = productName;
+            PictureURL = pictureURL;
+        }
+
+        public int ProductItemId { get; set; }
+        public string ProductName { get; set; }
+        public string PictureURL { get; set; }
+    }
+}
